c5f0dda44fab44b45a96e385c139971c39651097
Yoimer <ydrf19@gmail.com>
2017-05-03 13:18:36 -0400

    Connectded to Castillolk.com.ve using WiFiEsp library

--- esp8266-connectwifi-UNO/esp8266-connectwifi-UNO.ino
+++ esp8266-connectwifi-UNO/esp8266-connectwifi-UNO.ino
@@ -1,25 +1,17 @@
-/**
- * @example ConnectWiFi.ino
- * @brief The ConnectWiFi demo of library WeeESP8266. 
- * @author Wu Pengfei<pengfei.wu@itead.cc> 
- * @date 2015.03
- * 
- * @par Copyright:
- * Copyright (c) 2015 ITEAD Intelligent Systems Co., Ltd. \n\n
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version. \n\n
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+/*
+ WiFiEsp example: WebClient
 
-#include <SoftwareSerial.h>
+ This sketch connects to google website using an ESP8266 module to
+ perform a simple web search.
+
+ For more details see: http://yaab-arduino.blogspot.com/p/wifiesp-example-client.html
+*/
+
+#include "WiFiEsp.h"
+
+// Emulate mySerial on pins 2/3 if not present
+#ifndef HAVE_HWSERIAL1
+#include "SoftwareSerial.h"
 
 //The connection in ESP8266 should be like these: 
 //
@@ -29,7 +21,6 @@
 //ESP8266_VCC->3.3V
 //ESP8266_GND->GND
 
-
 //ESP8266_TX is connected to Arduino UNO as RX in D3
 #define ESP8266_RX_PIN 3
 
@@ -38,44 +29,97 @@
 
 //Create software serial object to communicate with ESP8266
 SoftwareSerial mySerial(ESP8266_RX_PIN, ESP8266_TX_PIN);
- 
-////SoftwareSerial mySerial(3, 2); /* RX:D3, TX:D2 */
 
-#include "ESP8266.h"
-#define SSID        "Casa"
-#define PASSWORD    "remioy2006202"
+////SoftwareSerial Serial1(6, 7); // RX, TX
+#endif
 
-//Create ESP8266 wifi object on mySerial to be able to use all ITEAD libraries
-ESP8266 wifi(mySerial);
+char ssid[] = "Casa";            // your network SSID (name)
+char pass[] = "remioy2006202";        // your network password
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
+////char server[] = "arduino.cc";
+char server[] = "castillolk.com.ve";
 
-void setup(void)
+// Initialize the Ethernet client object
+WiFiEspClient client;
+
+void setup()
 {
-    Serial.begin(9600);
-    Serial.print("setup begin\r\n");
+  // initialize serial for debugging
+  Serial.begin(115200);
+  // initialize serial for ESP module
+  mySerial.begin(9600);
+  // initialize ESP module
+  WiFi.init(&mySerial);
 
-    Serial.print("FW Version: ");
-    Serial.println(wifi.getVersion().c_str());
-    
-    
-    if (wifi.setOprToStation()) {
-        Serial.print("to station ok\r\n");
-    } else {
-        Serial.print("to station err\r\n");
-    }
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true);
+  }
 
-    if (wifi.joinAP(SSID, PASSWORD)) {
-        Serial.print("Join AP success\r\n");
-        Serial.print("IP: ");       
-        Serial.println(wifi.getLocalIP().c_str());
-    } else {
-        Serial.print("Join AP failure\r\n");
-    }
-    
-    Serial.print("setup end\r\n");
+  // attempt to connect to WiFi network
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network
+    status = WiFi.begin(ssid, pass);
+  }
+
+  // you're connected now, so print out the data
+  Serial.println("You're connected to the network");
+  
+  printWifiStatus();
+
+  Serial.println();
+  Serial.println("Starting connection to server...");
+  // if you get a connection, report back via serial
+  if (client.connect(server, 80)) {
+    Serial.println("Connected to server");
+    // Make a HTTP request
+    client.println("GET /WhiteList.txt HTTP/1.1");
+    client.println("Host: castillolk.com.ve");
+    client.println("Connection: close");
+    client.println();
+  }
 }
 
-void loop(void)
+void loop()
 {
+  // if there are incoming bytes available
+  // from the server, read them and print them
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
+
+  // if the server's disconnected, stop the client
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("Disconnecting from server...");
+    client.stop();
+
+    // do nothing forevermore
+    while (true);
+  }
 }
 
+
+void printWifiStatus()
+{
+  // print the SSID of the network you're attached to
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength
+  long rssi = WiFi.RSSI();
+  Serial.print("Signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
